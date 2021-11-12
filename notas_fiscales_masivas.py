548fa7d3d9f65e5f06760850f3871995a577bd97
Hernandezjose2000 <jhernandeza@fi.uba.ar>
2021-11-12 19:01:01 -0300

    primer commit

---
+++ notas_fiscales_masivas.py
@@ -0,0 +1,94 @@
+import requests
+import os
+import threading
+
+def obtener_info_shipments(listado_shipments:list, shipments_sin_info:list, info_shipments_id:list):
+
+    '''
+    PRE:Recibimos como listas los shipmets y una lista vacia donde se agregan los shipments que no se pueda obtener informacion.
+    POST:Retornamos la lista con la info necesitada de cada shipment.
+    '''
+    
+    info_shipments = 0
+    
+    for shipment_id in listado_shipments:
+
+        try:
+            extraer_info_shipments = requests.get(f"http://internal-api.mercadolibre.com/shipments/{shipment_id}/invoice_data/all?siteId=MLB&caller.scopes=admin&caller.id=admin")
+            info_parseada = extraer_info_shipments.json()
+            info_shipments_id.append((shipment_id,info_parseada[info_shipments]['sender_id'], info_parseada[info_shipments]['fiscal_key'], "pdf"))
+            print(f"{shipment_id} - Informacion obtenida")
+        except Exception:
+            shipments_sin_info.append(shipment_id)
+
+    
+def obtener_listado_shipments(nombre_archivo:str) ->list:
+    
+    '''
+    PRE:Recibe el nombre del archivo como string.
+    POST:Retornamos en una lista los shipments id.    
+    '''
+    listado_shipments_limpios = list()
+
+    with open(nombre_archivo,"r") as archivo:
+        for shipment_id in archivo:
+            listado_shipments_limpios.append(shipment_id.strip("\n"))
+    
+    return listado_shipments_limpios
+
+
+def obtener_notas(listado_info_shipments:list, nombre_carpeta:str, shipments_sin_info:list) ->None:
+
+    """
+    PRE:Recibimos como lista todos los shipments ademas de el nombre de la carpeta donde se descargaran las NF de los SH.
+    POST:Al ser un procedimiento, se retorna un dato de tipo None.
+    """
+    os.mkdir(nombre_carpeta)
+    os.system("cls")
+    
+    for shipment, sender_id, fiscal_key, type in listado_info_shipments:
+
+        try:
+            consulta = requests.get(f"https://internal-api.mercadolibre.com/shipping-tax/gateway/shipments/{shipment}/nfe/{fiscal_key}?doctype={type}&caller.id={sender_id}&caller.scopes=admin")
+            with open(f"{nombre_carpeta}\\{shipment}.pdf","wb")as archivo:
+                archivo.write(consulta.content)
+                print(f"{shipment} - Nota fiscal descargada")
+        except Exception:
+            shipments_sin_info.append(shipment)
+
+
+def mostrar_notas_no_descargadas(shipments_sin_info:list) ->None:
+
+    '''
+    PRE:Recibimos una lista de los shipments sin notas fiscales.
+    POST:Al ser procedimiento retornamos un tipo de dato None.
+    '''
+    vacio = 0
+    os.system("cls")
+    
+
+    if len(shipments_sin_info) == vacio:
+        print("Todas las notas se pudieron descargar correctamente.")
+    else:
+        print("SHIPMETS QUE NO SE PUDO DESCARGAR SUS NOTAS FISCALES.")
+    
+        for shipment_id in shipments_sin_info:
+            print(shipment_id)
+
+
+def main():
+
+    info_shipments = list()
+    shipments_sin_info = list()
+    listado_shipments = obtener_listado_shipments("shipments.txt")
+    for i in range (len(listado_shipments)):
+        hilo = threading.Thread(target=obtener_info_shipments, args=[listado_shipments, shipments_sin_info, info_shipments])
+        hilo.start()
+        print(info_shipments)
+    #obtener_info_shipments(listado_shipments, shipments_sin_info, info_shipments)
+    
+    #obtener_notas(info_shipments,"NOTAS FISCALES", shipments_sin_info)
+    #mostrar_notas_no_descargadas(shipments_sin_info)
+
+
+main()
